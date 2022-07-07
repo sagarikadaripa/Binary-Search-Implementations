ea0e9904ab0a23c75513a65b565412d15191b06b
sagarikadaripa <sagarikadaripa@gmail.com>
2022-07-08 00:13:47 +0530

    basic questions on array from gfg

---
+++ checkSum.java
@@ -0,0 +1,65 @@
+/* Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr whose sum is exactly X.
+
+Example 1:
+
+Input:
+N = 6, X = 16
+Arr[] = {1, 4, 45, 6, 10, 8}
+Output: Yes
+Explanation: Arr[3] + Arr[4] = 6 + 10 = 16
+Example 2:
+
+Input:
+N = 5, X = 10
+Arr[] = {1, 2, 4, 3, 6}
+Output: Yes
+Explanation: Arr[2] + Arr[4] = 4 + 6 = 10
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function hasArrayTwoCandidates() which takes the array of integers arr, n and x as parameters and returns boolean denoting the answer.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N) */
+import java.util.*;
+import java.io.*;
+
+public class checkSum {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().hasArrayTwoCandidates(arr, n, x);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        // code here
+        Arrays.sort(arr);
+        
+        int l = 0, h = n - 1;
+        while(l < h){
+            if(arr[l] + arr[h] == x) return true;
+            else if(arr[l] + arr[h] > x) h--;
+            else l++;
+        }
+        return false;
+    }
+} 
